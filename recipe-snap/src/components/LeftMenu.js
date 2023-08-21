0ed835abc9816feb4da7a9e51e884bb05a2043a9
Terry Nguyen <terrynguyen100@gmail.com>
2023-08-20 20:59:39 -0600

    add sample link buttons

--- recipe-snap/src/components/LeftMenu.js
+++ recipe-snap/src/components/LeftMenu.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
-
+import Button from '@mui/material/Button';
 
 const LeftMenu = ({ onUrlSubmit }) => {
   const [url, setUrl] = useState('');
@@ -9,8 +9,21 @@ const LeftMenu = ({ onUrlSubmit }) => {
     setUrl(event.target.value);
   };
 
+  const handleSample1Click = () => {
+    setUrl('https://www.inspiredtaste.net/37475/homemade-chicken-noodle-soup-recipe/');
+  };
+
+  const handleSample2Click = () => {
+    setUrl('https://www.inspiredtaste.net/41722/roasted-fingerling-potatoes/');
+  };
+  const handleSample3Click = () => {
+    setUrl('https://www.inspiredtaste.net/18596/soft-and-chewy-oatmeal-raisin-cookie-recipe/');
+  };
+
   const handleSubmit = () => {
-    onUrlSubmit(url);
+    if (url !== '') {
+      onUrlSubmit(url);
+    }
   };
 
   return (
@@ -22,7 +35,18 @@ const LeftMenu = ({ onUrlSubmit }) => {
         value={url}
         onChange={handleUrlChange}
       />
-      <button onClick={handleSubmit}>Submit</button>
+      <Button variant="contained" onClick={handleSample1Click} style={{ marginRight: '8px', marginTop: '10px' }}>
+        Sample 1
+      </Button>
+      <Button variant="contained" onClick={handleSample2Click} style={{ marginRight: '8px', marginTop: '10px' }}>
+        Sample 2
+      </Button>
+      <Button variant="contained" onClick={handleSample3Click} style={{ marginRight: '8px', marginTop: '10px' }}>
+        Sample 3
+      </Button>
+      <Button variant="contained" color="error" onClick={handleSubmit} style={{ marginRight: '8px', marginTop: '10px' }}>
+        Submit
+      </Button>
     </div>
   );
 };
