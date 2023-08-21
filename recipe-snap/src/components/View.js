26e8057ec93caeb1cf43ee19b9fdc44cbbf08637
Terry Nguyen <terrynguyen100@gmail.com>
2023-08-20 21:00:00 -0600

    add view to handle array

--- recipe-snap/src/components/View.js
+++ recipe-snap/src/components/View.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import { List, ListItem, ListItemText, Typography } from '@mui/material';
 
 const View = ({ response }) => {
-  console.log(response)
+  console.log(response);
+
+  let ingredientList = null;
+  let instructionList = null;
+
+  if (response !== '') {
+    ingredientList = response.ingredients.map((ingredient, index) => (
+      <ListItem key={index}>
+        <ListItemText primary={ingredient} />
+      </ListItem>
+    ));
+
+    instructionList = response.instructions.map((instruction, index) => (
+      <ListItem key={index}>
+        <ListItemText primary={instruction} />
+      </ListItem>
+    ));
+  }
   return (
     <div>
-      <h2>Response:</h2>
-      <p>{response.content}</p>
+      {ingredientList && (
+        <div>
+          <Typography variant="h4">Ingredients:</Typography>
+          <List>{ingredientList}</List>
+        </div>
+      )}
+      {instructionList && (
+        <div>
+          <Typography variant="h4">Instructions:</Typography>
+          <List>{instructionList}</List>
+        </div>
+      )}
     </div>
   );
 };
