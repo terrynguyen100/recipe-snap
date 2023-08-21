d918cd0175346e0729d927d0f296d4e30d347818
Terry Nguyen <terrynguyen100@gmail.com>
2023-08-20 18:10:40 -0600

    move openai_req to a separate file

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,36 @@
 from fastapi import FastAPI
-import configparser
-import openai
 from scrape_recipe import scrape_recipe
+from openai_req import get_openai_response
 
 app = FastAPI()
 
-# Load OpenAI API key from config.ini
-config = configparser.ConfigParser()
-config.read("config.ini")
-openai_api_key = config.get("openai", "api_key")
-
-# Set OpenAI API key
-openai.api_key = openai_api_key
-
-
 sample_url = "https://www.inspiredtaste.net/37475/homemade-chicken-noodle-soup-recipe/"
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/get_recipe")
 async def get_recipe():
     try:
         metadata = scrape_recipe(sample_url)
-        return {"Metadata": metadata}
+        ingredients = metadata["recipeIngredient"]
+
+        prompt = (
+            """
+            Here is the list of ingredients with each line is 1 group of ingredient. Each line will be formatted into a key-value pair.
+            Analyze each line and format them into a JSON format following this sample: if the line is "2 tablespoons butter, chicken fat or olive oil", then the JSON format will be {"butter, chicken fat or olive oil" : "2 tablespoons}.
+            Remember that each line will result in only 1 key-value pair.
+            Return the entire list of ingredients as a JSON such as "ingredients" : {}.
+
+            List of ingredients:\n 
+            """ +
+            "\n".join(ingredients)
+        )
+        extracted_info = get_openai_response(prompt)
+
+        return {extracted_info}
     except Exception as e:
         return {"Error": str(e)}
--- backend/openai_req.py
+++ backend/openai_req.py
@@ -8,13 +8,15 @@ openai_api_key = config.get("openai", "api_key")
 
 # Set OpenAI API key
 openai.api_key = openai_api_key
+models = openai.Model.list()
 
-def openai_req(prompt):
+def get_openai_response(prompt):
     try:
+      
         response = openai.Completion.create(
-            engine="gpt-3.5-turbo",
+            engine="text-davinci-001",
             prompt=prompt,
-            max_tokens=1000
+            max_tokens=1000  # You can adjust this value based on your needs
         )
         return response.choices[0].text
     except Exception as e:
