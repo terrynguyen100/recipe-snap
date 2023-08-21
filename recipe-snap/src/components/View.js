5f13a62c8ef5a92cbe88c918d8bba1a010cae318
Terry Nguyen <terrynguyen100@gmail.com>
2023-08-20 21:15:15 -0600

    show an error if no metadata found

--- recipe-snap/src/components/View.js
+++ recipe-snap/src/components/View.js
@@ -2,38 +2,43 @@ import React from 'react';
 import { List, ListItem, ListItemText, Typography } from '@mui/material';
 
 const View = ({ response }) => {
-  console.log(response);
+  let ingredientList = [];
+  let instructionList = [];
 
-  let ingredientList = null;
-  let instructionList = null;
-
-  if (response !== '') {
+  if (response.ingredients) {
     ingredientList = response.ingredients.map((ingredient, index) => (
       <ListItem key={index}>
         <ListItemText primary={ingredient} />
       </ListItem>
     ));
+  }
 
+  if (response.instructions) {
     instructionList = response.instructions.map((instruction, index) => (
       <ListItem key={index}>
         <ListItemText primary={instruction} />
       </ListItem>
     ));
   }
+
   return (
     <div>
-      {ingredientList && (
+      {ingredientList.length > 0 && (
         <div>
           <Typography variant="h4">Ingredients:</Typography>
           <List>{ingredientList}</List>
         </div>
       )}
-      {instructionList && (
+      {instructionList.length > 0 && (
         <div>
           <Typography variant="h4">Instructions:</Typography>
           <List>{instructionList}</List>
         </div>
       )}
+      {instructionList.length === 0 && ingredientList.length === 0 && (
+        <Typography variant="h4" color="error">No data found for this URL</Typography>
+      )}
+
     </div>
   );
 };
