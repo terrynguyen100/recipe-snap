b3730447c30b659a2005f93fe6b2cd22a449930e
Terry Nguyen <terrynguyen100@gmail.com>
2023-08-20 20:12:30 -0600

    add ingredients, instructions, recipe route

--- backend/main.py
+++ backend/main.py
@@ -1,36 +1,55 @@
 from fastapi import FastAPI
 from scrape_recipe import scrape_recipe
 from openai_req import get_openai_response
+from helper.helper import find_recipe_key
 
 app = FastAPI()
 
-sample_url = "https://www.inspiredtaste.net/37475/homemade-chicken-noodle-soup-recipe/"
-
-
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"Hello" "World"}
 
+        # prompt = (
+        #     """
+        #     Here is the list of ingredients with each line is 1 group of ingredient. Each line will be formatted into a key-value pair.
+        #     Analyze each line and format them into a JSON format following this sample: if the line is "2 tablespoons butter, chicken fat or olive oil", then the JSON format will be "butter, chicken fat or olive oil" : "2 tablespoons.
+        #     Remember that each line will result in only 1 key-value pair.
+        #     Return the entire list of ingredients as a JSON such as "ingredients" : {}.
+
+        #     List of ingredients:\n 
+        #     """ +
+        #     "\n".join(ingredients)
+        # )
+        # extracted_info = get_openai_response(prompt)
+
+@app.get("/get_ingredients")
+async def get_ingredients(url: str):
+    try:
+        metadata = scrape_recipe(url)
+        ingredients = find_recipe_key(metadata, "recipeIngredient")
+        return {"ingredients": ingredients}
+    except Exception as e:
+        return {"Error": str(e)}
+
+
+@app.get("/get_instructions")
+async def get_instructions(url: str):
+    try:
+        metadata = scrape_recipe(url)
+        instructions = find_recipe_key(metadata, "recipeInstructions")
+        return {"instructions": instructions}
+    except Exception as e:
+        return {"Error": str(e)}
 
 @app.get("/get_recipe")
-async def get_recipe():
+async def get_recipe(url: str):
     try:
-        metadata = scrape_recipe(sample_url)
-        ingredients = metadata["recipeIngredient"]
-
-        prompt = (
-            """
-            Here is the list of ingredients with each line is 1 group of ingredient. Each line will be formatted into a key-value pair.
-            Analyze each line and format them into a JSON format following this sample: if the line is "2 tablespoons butter, chicken fat or olive oil", then the JSON format will be "butter, chicken fat or olive oil" : "2 tablespoons.
-            Remember that each line will result in only 1 key-value pair.
-            Return the entire list of ingredients as a JSON such as "ingredients" : {}.
-
-            List of ingredients:\n 
-            """ +
-            "\n".join(ingredients)
-        )
-        extracted_info = get_openai_response(prompt)
-
-        return extracted_info
+        metadata = scrape_recipe(url)
+        ingredients = find_recipe_key(metadata, "recipeIngredient")
+        instructions = find_recipe_key(metadata, "recipeInstructions")
+        return {  
+                  "ingredients": ingredients,
+                  "instructions": instructions,
+                }
     except Exception as e:
         return {"Error": str(e)}
